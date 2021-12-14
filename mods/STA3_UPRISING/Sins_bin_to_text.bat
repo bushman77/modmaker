be5311cba93bf1e9f9baf7ea82683a4392b9d692
bushman77 <banolik@gmail.com>
2021-12-14 07:57:16 -0800

    fixed untracked files

--- mods/STA3_UPRISING/Sins_bin_to_text.bat
+++
@@ -1,4 +0,0 @@
-for %%a in (*.entity) do ConvertData_Rebellion entity %%a %%a txt
-for %%a in (*.particle) do ConvertData_Rebellion particle %%a %%a txt
-for %%a in (*.mesh) do ConvertData_Rebellion mesh %%a %%a txt
-for %%a in (*.brushes) do ConvertData_Rebellion brushes %%a %%a txt
